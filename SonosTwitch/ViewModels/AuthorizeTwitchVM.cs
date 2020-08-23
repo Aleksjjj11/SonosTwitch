99d867ca925552e110950f9bcf97a67c1773cbf8
aleksjjj11 <aleksjjj11@fex.net>
2020-08-23 19:40:51 +0500

    Add binding in AuthorizeTwitch.xaml

--- SonosTwitch/ViewModels/AuthorizeTwitchVM.cs
+++ SonosTwitch/ViewModels/AuthorizeTwitchVM.cs
@@ -2,6 +2,13 @@
 {
     public class AuthorizeTwitchVM
     {
-        
+        public string Login { get; set; }
+        public string Token { get; set; }
+
+        public AuthorizeTwitchVM()
+        {
+            Login = App.Setting.TwitchChannel;
+            Token = App.Setting.TwitchToken;
+        }
     }
 }
